47b2a71e07851040d13363cc6b8662ce09d0dd1b
PavloShutz <99386195+PavloShutz@users.noreply.github.com>
2022-07-09 17:29:23 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ GREETING_MESSAGE = """
 I'm file converter bot. I would help you to convert files of different formats.\n
 Basicly, just send me a file, and then, in the inline keyboard you will choose available file extensions for your file.
 """
-TOKEN = '5238542346:AAGvN_VbBtT2clIP_NTYFxvah9Kf-E9uwvU'
+TOKEN = 'bot token'
 AVAILABLE_FORMATS = {
 	'csv':	['csv', 'jpg', 'pdf', 'pdfa', 'png', 
 		'tiff', 'webp', 'xls', 'xlsx'],
 	'ico':	['jpg', 'pdf', 'pdfa', 'png', 
 		'svg', 'tiff', 'webp']
 }
-SECRET = 'inA90W4czyFbrojQ'
+SECRET = 'secret key for convertapi'
