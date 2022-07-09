dfb4c9857347c41edf7d6043c43cb29063a94659
PavloShutz <99386195+PavloShutz@users.noreply.github.com>
2022-07-09 17:29:32 +0300

    Delete logic.py

--- logic.py
+++
@@ -1,8 +0,0 @@
-import convertapi
-convertapi.api_secret = 'inA90W4czyFbrojQ'
-
-result = convertapi.convert('doxc', { 'File': r'C:\Users\Lenovo\Desktop\test.txt' })
-
-print(result)
-# save to file
-result.file.save(r'F:\Python\File Converter\test.docx')
